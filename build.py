5cd78cae50332bd98bb6bbd851eeb256132792d9
Alireza <alirezah320@yahoo.com>
2021-10-04 19:01:07 +0330

    Add publish settings

---
+++ build.py
@@ -0,0 +1,22 @@
+"""
+Build script for resume HTML version.
+"""
+import os
+import shutil
+from os.path import isdir, join
+
+from portfolio.app import write_html
+
+os.makedirs('site', exist_ok=True)
+shutil.rmtree('site')
+os.makedirs('site', exist_ok=True)
+
+for file in os.listdir(join('portfolio', 'templates')):
+    write_html(name=file[:-5], path=join('site', file))
+
+static_dir = join('portfolio', 'static')
+for filename in os.listdir(static_dir):
+    filepath = join(static_dir, filename)
+    if filename != 'scss' and isdir(filepath):
+        shutil.copytree(filepath,
+                        join('site', 'static', filename))
--- portfolio/app.py
+++ portfolio/app.py
@@ -6,14 +6,37 @@ from os.path import join
 import yaml
 from dict_deep import deep_get
 from flask import Flask, Markup, render_template
+from jinja2 import Template
 from markdown import markdown as md
 
 app = Flask(__name__)
 
 
+def render_template_2(path: str, data: dict) -> str:
+    """
+    Renders a template with given data.
+
+    Parameters
+    ----------
+    path: str
+        Path to the template
+    data:
+        The provided data for rendering
+
+    Returns
+    -------
+    template: str
+        Rendered template
+    """
+    with open(path, 'r+') as f:
+        template = f.read()
+    template = Template(source=template)
+    return template.render(data=data)
+
+
 def make_html(name: str) -> str:
     """
-    Makes HTML with provided data
+    Makes HTML with provided data.
 
     Parameters
     ----------
@@ -28,6 +51,24 @@ def make_html(name: str) -> str:
     data = read_data(name)
     html = render_template(f'{name}.html', data=data)
     return html
+
+
+def write_html(name: str, path: str) -> None:
+    """
+    Write the HTML to disk.
+
+    Parameters
+    ----------
+    name: str
+        Name of the HTML
+
+    path: str
+        Path to write HTML to
+    """
+    data = read_data(name)
+    html = render_template_2(join('portfolio', 'templates', f'{name}.html'), data)
+    with open(path, 'w+') as f:
+        f.write(html)
 
 
 def read_data(name: str, markup: bool = True) -> dict:

fedf41eb9fe3dc558b7dc5f61d1163e7cdc020f0
Alireza <alirezatheh@gmail.com>
2021-10-16 04:38:41 +0330

    Update latex action

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 python build.py
 cd portfolio/tex || exit
 mkdir -vp build
-latexmk -pdf --interaction=batchmode --output-directory=build resume.tex
+latexmk -pdf -interaction=batchmode -output-directory=build resume.tex
 cd ../..
 mv portfolio/tex/build/resume.pdf site/files
 rm -rf portfolio/tex/build
